d640950976f9fa319779ff68ccaa85633fe4719f
PremerX007 <39229888+PremerX007@users.noreply.github.com>
2023-02-21 17:18:55 +0700

    init

--- lab20_2.cpp
+++ lab20_2.cpp
@@ -5,8 +5,9 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect *a, Rect *b){
+	double xpo = min(a->x+a->w,b->x+b->w) - max(a->x,b->x);
+	double ypo = min(a->y,b->y) - max(a->y-a->h,b->y-b->h);
+	if(xpo < 0 || ypo < 0) return 0;
+	else return xpo*ypo;
 }
--- lab20_3.cpp
+++ lab20_3.cpp
@@ -8,8 +8,10 @@
 using namespace std;
 
 struct student{
-
-    //Define struct student with four member (name ,id , gender, gpa);
+	string name;
+	int id;
+	char gender;
+	float gpa;
     
 };
 
@@ -20,7 +22,7 @@ struct course{
 	vector<student *> student_list;
 };
 
-student * findstudent(vector<student> allstudents,int key){ //There is something wrong in this line.
+student * findstudent(vector<student> &allstudents,int key){
 	for(unsigned int i = 0; i < allstudents.size(); i++){
 		if(allstudents[i].id  == key) return &allstudents[i];
 	}
@@ -63,13 +65,14 @@ int main(){
 	
 	while(getline(student_file,textline)){
 		student s; 
-		
-		//Use sscanf() to split the values in textline and assign those values to the members of struct s;
-
-		allstudents.push_back(s); 		
+		char temp[100];
+		char format[] = "%[^,],%d,%[^,],%f";
+		sscanf(textline.c_str(),format,temp,&s.id,&s.gender,&s.gpa);
+		s.name = temp;
+		allstudents.push_back(s); 
 	}
 	
-	int state = 1;
+	int state = 1, s = 0;
 	while(getline(course_file,textline)){
 		if(state == 1){
 			course c;
@@ -83,17 +86,15 @@ int main(){
 			if(textline == "> Students"){
 				state = 3;
 			}else{
-			
-			    //Append (push_back) textline to lecture_list[] of the recently added course in allcourses[];
-			    
+				allcourses[s].lecture_list.push_back(textline);
 			}			
 		}else{
 			if(textline == "---------------------------------------"){
 				state = 1;
+				s++;
 			}else{
 				student *p = findstudent(allstudents,atof(textline.c_str()));
-				
-				//Append (push_back) p to student_list of the recently added course in allcourses[];
+				allcourses[s].student_list.push_back(p);
 				
 			}
 		}
