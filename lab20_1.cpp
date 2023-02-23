511240c37522b073368657b5434f0e35b68929d5
PremerX007 <39229888+PremerX007@users.noreply.github.com>
2023-02-23 20:25:19 +0700

    completed

--- lab20_1.cpp
+++ lab20_1.cpp
@@ -4,8 +4,8 @@ using namespace std;
 struct Rect{
 	double x,y,w,h;
 };
-
-double overlap(Rect a, Rect b){
+                            
+double overlap(Rect a, Rect b){                    
 	double xpo = min(a.x+a.w,b.x+b.w) - max(a.x,b.x);
 	double ypo = min(a.y,b.y) - max(a.y-a.h,b.y-b.h);
 	if(xpo < 0 || ypo < 0) return 0;
